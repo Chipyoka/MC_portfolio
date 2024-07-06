c4c680fc825ec9a9fe56575935a52cd30c3d32a0
Chipyoka Mwape <mwapechipyoka18@gmail.com>
2024-07-06 15:25:09 +0200

    Added slide in notification

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+
+window.onload = function () {
+	const notification = document.getElementById("notification");
+	notification.classList.add("show");
+
+	// code for hiding the notification after 5 seconds
+	setTimeout(() => {
+		notification.classList.remove("show");
+	}, 8000);
+};
